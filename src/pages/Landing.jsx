38eb2c0538c27f6a68b893a739983df23094b9b2
Samiksha Chandel <jennachandel14@gmail.com>
2026-05-30 16:09:24 +0530

    Updated demo video link

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -59,7 +59,7 @@ export default function Landing() {
               <Button size="lg" onClick={() => user ? navigate('/products') : navigate('/login')} className="group">
                 Start Trying On <ArrowRight className="w-5 h-5 ml-2 group-hover:translate-x-1 transition-transform" />
               </Button>
-              <Button variant="secondary" size="lg" onClick={() => window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")}><Play className="w-5 h-5 mr-2" />Watch Demo</Button>
+              <Button variant="secondary" size="lg" onClick={() => window.open("https://youtu.be/m4jUq0fsEys", "_blank")}><Play className="w-5 h-5 mr-2" />Watch Demo</Button>
             </div>
           </motion.div>
         </div>
